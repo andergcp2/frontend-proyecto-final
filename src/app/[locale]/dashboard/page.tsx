9cd3ebb55e679f1f0597ba93942ed79135489fbf
andergcp <andergcp@hotmail.com>
2023-10-15 21:56:41 -0500

    Fix build

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const t = useTranslations('Index');
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Typography variant="h1" component="h1">{t('title')}</ Typography >
+      <Typography variant="h1" component="h1">{t('title')} - Prueba</ Typography >
     </div>
   )
 }
