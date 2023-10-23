ddafbea149ce6e1ea572effce45afaad5d84fe1c
andergcp <andergcp@hotmail.com>
2023-10-22 22:35:57 -0500

    Fix E2E Types

---
+++ cypress/e2e/companies-spec.cy.js
@@ -0,0 +1,41 @@
+// https://docs.cypress.io/guides/references/best-practices
+// Best Practice: Use data-* attributes to provide context to your selectors and isolate them from CSS or JS changes.
+
+describe("abcjobs - companies", () => {
+  context("Given an user company accesses abcjobs register page", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000/es/auth/register/company");
+      // cy.wait(1000)
+    });
+
+    context("When user company see register page", () => {
+      it("Then he/she sees register company form", () => {
+        cy.checkCompanyTest();
+      });
+    });
+
+    context(
+      "When user company see register page, no input data and click submit button",
+      () => {
+        it("Then he/she sees error messages", () => {
+          cy.createCompanyTestEmpty();
+        });
+      }
+    );
+
+    context(
+      "When user company see register page, input all data and click submit button",
+      () => {
+        it("Then company is created and sees confirmation message", () => {
+          cy.createCompanyTestOK();
+        });
+      }
+    );
+
+    // context('When user company see register page, input all data with different passwords and click submit button', () => {
+    //   it('Then he/she sees password error message', () => {
+    //     cy.createCompanyTestPwdError()
+    //   })
+    // })
+  });
+});
