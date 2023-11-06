1dd8194dbdacba1ab8013b754c0b7b174a917cd2
magon-miso <m.agonf@uniandes.edu.co>
2023-11-05 21:28:27 -0500

    fix messages given-when-then companies e2e tests

--- cypress/e2e/companies-spec.cy.js
+++ cypress/e2e/companies-spec.cy.js
@@ -2,20 +2,20 @@
 // Best Practice: Use data-* attributes to provide context to your selectors and isolate them from CSS or JS changes.
 
 describe("abcjobs - companies", () => {
-  context("Given an user company accesses abcjobs register page", () => {
+  context("Given an company's user access abcjobs register's page", () => {
     beforeEach(() => {
       cy.visit("http://localhost:3000/es/auth/register/company");
-      // cy.wait(1000)
+      cy.wait(1000)
     });
 
-    context("When user company see register page", () => {
-      it("Then he/she sees register company form", () => {
+    context("When company's user sees register's page", () => {
+      it("Then he/she sees register's company form", () => {
         cy.checkCompanyTest();
       });
     });
 
     context(
-      "When user company see register page, no input data and click submit button",
+      "When user company see register's page, no input data and click submit button",
       () => {
         it("Then he/she sees error messages", () => {
           cy.createCompanyTestEmpty();
@@ -24,7 +24,7 @@ describe("abcjobs - companies", () => {
     );
 
     context(
-      "When user company see register page, input all data and click submit button",
+      "When user company see register's page, input all data and click submit button",
       () => {
         it("Then company is created and sees confirmation message", () => {
           cy.createCompanyTestOK();
