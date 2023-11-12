31e8638543a480428b933184f993435f83633f38
csolanor22 <c.solanor@uniandes.edu.co>
2023-11-12 01:06:27 -0500

    Add: logo for ABC jobs

--- src/app/[locale]/auth/login/components/LoginForm.tsx
+++ src/app/[locale]/auth/login/components/LoginForm.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { FC } from 'react'
-import { Box, TextField, FormControlLabel, Checkbox, Typography, FormControl, InputLabel, Select, MenuItem } from '@mui/material'
+import Business from '@mui/icons-material/Business';
+import { Box, TextField, Avatar , Paper, Typography, Grid , Container, Select, MenuItem } from '@mui/material'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import { EmailInput, PasswordInput } from '@/components'
@@ -34,75 +35,83 @@ const LoginForm: FC<LoginFormProps> = () => {
   })
 
   return (
-    <Box
-      component="form"
-      onSubmit={formik.handleSubmit}
-      id="login-form"
-      noValidate
-      sx={{ mt: 1, width: '50%' }}
-      display={'flex'}
-      flexDirection={'column'}
-      alignItems={'center'}
-      margin={'auto'}
-      pt={5}
-      maxWidth={'500px'}
-    >
-      <Typography variant="h1" py={5}>Iniciar sesión</Typography>
-      <TextField
-        id="username"
-        name="username"
-        label={t('username')}
-        fullWidth
-        required
-        value={formik.values.username}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        error={formik.touched.username && Boolean(formik.errors.username)}
-        helperText={
-          formik.touched.username &&
-          Boolean(formik.errors.username) &&
-          t(formik.errors.username as string)
-        }
-      />
-      <PasswordInput
-        id="password"
-        name="password"
-        label={t('password')}
-        margin="normal"
-        value={formik.values.password}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        error={
-          Boolean(formik.touched.password) && Boolean(formik.errors.password)
-        }
-        errorText={
-          formik.errors.password ? validationT(formik.errors.password) : ''
-        }
-      />
-
-      {/* <FormControl fullWidth>
-        <InputLabel id="userrole">Role</InputLabel>
-        <Select
-          labelId="select-role"
-          id="select-role"
-          value={formik.values.userRole}
-          label={t('userRole')}
-          onChange={formik.handleChange}
-        >
-          <MenuItem value={10}>Candidate</MenuItem>
-          <MenuItem value={20}>Company</MenuItem>
-          <MenuItem value={30}>ABC</MenuItem>
-        </Select>
-      </FormControl> */}
-      <Button
-        variant="contained"
-        type="submit"
-        sx={{ mt: 3, mb: 2, fontWeight: 600, maxWidth: '50%' }}
-      // loading={isLoginLoading}
-      >
-        {t('signInBtn')}
-      </Button>
-    </Box>
+    <Container component="main" maxWidth="xs">
+      <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+        <Box component="img" src='/Logo.png' alt='Logo abc' sx={{ height: "auto", width: "100%" }} />
+          <Box
+            component="form"
+            onSubmit={formik.handleSubmit}
+            id="login-form"
+            noValidate
+            sx={{ mt: 1 }}
+            display={'flex'}
+            flexDirection={'column'}
+            alignItems={'center'}
+            margin={'auto'}
+            maxWidth={'500px'}
+          >
+            
+            <Typography component="h1" variant="h4" align="center" sx={{ mt: 3, mb: 3 }}>
+              {t('Title')}
+            </Typography>
+            <TextField
+              id="username"
+              name="username"
+              label={t('username')}
+              fullWidth
+              required
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.username && Boolean(formik.errors.username)}
+              helperText={
+                formik.touched.username &&
+                Boolean(formik.errors.username) &&
+                t(formik.errors.username as string)
+              }
+            />
+            <PasswordInput
+              id="password"
+              name="password"
+              label={t('password')}
+              margin="normal"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={
+                Boolean(formik.touched.password) && Boolean(formik.errors.password)
+              }
+              errorText={
+                formik.errors.password ? validationT(formik.errors.password) : ''
+              }
+            />
+            
+            {/* <FormControl fullWidth>
+              <InputLabel id="userrole">Role</InputLabel>
+              <Select
+                labelId="select-role"
+                id="select-role"
+                value={formik.values.userRole}
+                label={t('userRole')}
+                onChange={formik.handleChange}
+              >
+                <MenuItem value={10}>Candidate</MenuItem>
+                <MenuItem value={20}>Company</MenuItem>
+                <MenuItem value={30}>ABC</MenuItem>
+              </Select>
+            </FormControl> */}
+            <Button
+              variant="contained"
+              type="submit"
+              fullWidth
+              sx={{ mt: 3, mb: 2, fontWeight: 600 }}
+            // loading={isLoginLoading}
+            >
+              {t('signInBtn')}
+            </Button>
+          </Box>
+      </Paper>
+    </Container>
   )
 }
 
